02b8e13ab512a5d6ce126ce55a235da4fe04802c
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-11 14:58:16 +0500

    Example 008 in development

---
+++ Example008_FriendsAndaDog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFrendSpeed = 1;
+int secondFreindSpeed = 2;
+int dogSpeed = 5;
+int freind = 2;
+int T = 0;
+while (distance < 10)
+{
+    if (freind == 1)
+    {
+        T = distance / (firstFrendSpeed + dogSpeed);
+        freind = 1;
+    }
+    if (freind == 2)
+    {
+        T = distance / (secondFreindSpeed + dogSpeed);
+        freind = 2;
+    }
+
+    distance = distance - (firstFrendSpeed + secondFreindSpeed) * T;
+    count = count + 1;
+       
+    Console.Write("Кол-во раз пробежала собака:  ");
+    Console.WriteLine(count);
+}
