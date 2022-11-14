35b49854ce9b89c470a7fdfc914610072ce75d90
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-14 15:21:24 +0500

    add dz+Example008_Function complete

---
+++ Example_008_Function/Program.cs
@@ -0,0 +1,27 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 123;
+int b1 = 5275211;
+int c1 = 34;
+int a2 = 78;
+int b2 = 5229;
+int c2 = 347;
+int a3 = -123;
+int b3 = 35777;
+int c3 = 34;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+Console.WriteLine(max);
---
+++ Example_dz_FriendsAndaDog/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+Console.Clear();
+int count = 0;
+int distance = 10000;
+int firstFrendSpeed = 1;
+int secondFreindSpeed = 2;
+int dogSpeed = 5;
+int freind = 2;
+int T = 0;
+while (distance < 10)
+{
+    if (freind == 1)
+    {
+        T = distance / (firstFrendSpeed + dogSpeed);
+        freind = 2;
+    }
+    if (freind == 2)
+    {
+        T = distance / (secondFreindSpeed + dogSpeed);
+        freind = 1;
+    }
+
+    distance = distance - (firstFrendSpeed + secondFreindSpeed) * T;
+    count = count + 1;
+       
+    Console.Write("Кол-во раз пробежала собака:  ");
+    Console.WriteLine(count);
+}
