3d81f69f567625f67270bdc895d182d3c3049cd3
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-21 11:59:21 +0500

    Example 012 Methods fixed2

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -121,3 +121,38 @@ Console.WriteLine(newText);
 Console.WriteLine();
 newText = Replace(newText, 'С', 'с' );
 Console.WriteLine(newText);
+Console.WriteLine();
+
+// Задача сортировка одномерного массива, по возрастанию.
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 3 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
