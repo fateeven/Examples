316d587cb1cd5c47ab7055c06e65bc80498e35b9
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-21 12:31:04 +0500

    Example 012 Methods fixed1

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -69,3 +69,55 @@ Console.WriteLine(res1);
 Console.WriteLine(res2);
 Console.WriteLine(res3);
 Console.WriteLine(res4);
+
+
+// Цикл for Пример вывода таблицы умножения
+for (int i=2; i <= 10; i++)
+{
+    for (int j=2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}   "); // - Интерполяция строк
+    }
+    Console.WriteLine();
+}
+
+
+//Задача.
+// Дан текст.  В тесте  все пробелы заменить черточкми - ,
+// Маленькие буквы "к" заменить большими "К",
+// а большие "С"заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, -что, "
+            + "ежели вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю? ССС";
+
+// Пример работы:
+
+//string s = "qwerty"
+//            012345
+//s[3] = r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; 
+    
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result; // не забывать возвращать результат в 4 Виде.
+}
+string newText = Replace(text, ' ', '|' );
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К' );  
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с' );
+Console.WriteLine(newText);
